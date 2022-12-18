1638f1d0bd217b40566506f7b99ac3a147b5aa89
Juma Harun <mmbh4run@gmail.com>
2022-12-18 04:43:21 +0300

    added animate on scroll html css and js

---
+++ app.js
@@ -0,0 +1,17 @@
+// observer
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    // console.log(entry);
+
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    } else {
+      entry.target.classList.remove('show');
+    }
+  });
+});
+
+// dom elements
+const hiddenElements = document.querySelectorAll('.hidden');
+
+hiddenElements.forEach((el) => observer.observe(el));
